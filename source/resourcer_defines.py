98336de785dc8f8cccf8f10817bb33314de3c1c5
KonstantIMP <mihedovkos@gmail.com>
2021-01-07 17:54:30 +0300

    Work with UI and defines Done defines file(with program version) hello function was written(Display hello message with program info)

---
+++ source/resourcer_defines.py
@@ -0,0 +1,5 @@
+major_ver = '0'
+minor_ver = '0'
+build_num = '0'
+
+version_str = major_ver + '.' + minor_ver + '.' + build_num + " (BUILD : " + build_num + ')'
---
+++ source/user_interface.py
@@ -0,0 +1,12 @@
+import resourcer_defines
+
+def print_hello() :
+    print("    _  _      \t")
+    print("   | || | _   \t RESOURCER\tVersion : " + resourcer_defines.version_str)
+    print("  -| || || |  \t")
+    print("   | || || |- \t A simple program for creating arrays")
+    print("    \\_  || |  \t \t\tof data from file resources")
+    print("      |  _/   \t")
+    print("     -| | \\   \t Feedback email : mihedovkos@gmail.com")
+    print("      |_|-    \t Powered by KonstantIMP")
+    print("")
