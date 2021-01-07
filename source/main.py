96d3c87a2ed848b4aa907d9c608b986eeb381be8
KonstantIMP <mihedovkos@gmail.com>
2021-01-07 18:13:53 +0300

    Done input file path entering

--- source/main.py
+++ source/main.py
@@ -2,3 +2,5 @@ import user_interface
 
 if __name__ == "__main__" :
     user_interface.print_hello()
+
+    input_file = user_interface.get_input_file()
--- source/user_interface.py
+++ source/user_interface.py
@@ -1,5 +1,7 @@
 import resourcer_defines
 
+import os.path
+
 def print_hello() :
     print("    _  _      \t")
     print("   | || | _   \t RESOURCER\tVersion : " + resourcer_defines.version_str)
@@ -10,3 +12,20 @@ def print_hello() :
     print("     -| | \\   \t Feedback email : mihedovkos@gmail.com")
     print("      |_|-    \t Powered by KonstantIMP")
     print("")
+
+def get_input_file() :
+    input_path = input("  Enter the path to the input file : ")
+
+    while True :
+        if os.path.exists(input_path) == True :
+            if os.path.isfile(input_path) == True :
+                print("")
+                return input_path
+            else :
+                print("")
+                print("  [ERROR] File \'" + input_path + "\' exists, but it is a FOLDER")
+                input_path = input("  Enter the path to the input file : ")
+        else :
+            print("")
+            print("  [ERROR] File \'" + input_path + "\' doesn`t exist")
+            input_path = input("  Enter the path to the input file : ")
